06a4d0d672512b33c2dc58e90b0d8a83adeb26ac
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-04 17:02:05 +0100

    Creato algoritmo base: - Il computer crea un array di 5 numeri diversi - L'user inserisce 5 numeri - Il programma verifica se i numeri inseriti dall'utente combaciano con quelli della cpu e li restituisci sottoforma di un altro array

--- js/main.js
+++ js/main.js
@@ -1,7 +1,51 @@
+"use strict";
+
 /* Descrizione:
 Visualizzare in pagina 5 numeri casuali da 1 a 100 senza duplicati.
 Da lì parte un timer di 30 secondi.
 Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno 
-alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+alla volta, i numeri che ha visto precedentemente, tramite il 
+prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti 
 e quali dei numeri da indovinare sono stati individuati. */
+
+/*--------------
+    FUNCTIONS   
+---------------*/
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+/*--------------
+    MAIN   
+---------------*/
+const cpuNumbers = [];
+const container = document.getElementById("container");
+const userNumbers = [];
+const foundNumbers = [];
+
+while(cpuNumbers.length < 5){
+    let generator = getRndInteger(1, 100);
+    if(!cpuNumbers.includes(generator)){
+        cpuNumbers.push(generator);
+    }
+}
+
+console.log(`Numeri da ricordare: ${cpuNumbers}`);
+container.append(`Memorizza questi numeri! ${cpuNumbers}`);
+
+for (let i = 1; i <= 5; i++) {
+    let userInput = Number(prompt("Inserisci un numero"));
+    userNumbers.push(userInput);
+}
+
+container.append(`Numeri inseriti: ${userNumbers}`);
+
+
+for (let i = 0; i < 5; i++) {
+    if(cpuNumbers.includes(userNumbers[i])){
+        foundNumbers.push(userNumbers[i]);
+    }
+}
+
+console.log(foundNumbers);
