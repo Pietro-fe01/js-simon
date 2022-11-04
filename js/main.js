9b186b4dfa5de2ab6b052f17ccb19bbe53a8128b
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-04 18:16:35 +0100

    Aggiunto un pulsante che al click genera la stringa e fa partire i 30 secondi

--- js/main.js
+++ js/main.js
@@ -12,63 +12,79 @@ e quali dei numeri da indovinare sono stati individuati. */
 /*--------------
     FUNCTIONS   
 ---------------*/
+// Funzione che genera un numero random compreso tra min e max (inclusi)
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// Funzione genera n numeri random e li inserisce in un array
+function numberGenerator(toGenerate){
+    let numberArray = [];
+    while(numberArray.length < toGenerate){
+        let generator = getRndInteger(1, 100);
+        if(!numberArray.includes(generator)){
+            numberArray.push(generator);
+        }
+    }
+    return numberArray;
 }
 
 /*--------------
     MAIN   
 ---------------*/
-const cpuNumbers = [];
-const container = document.getElementById("container");
-const userNumbers = [];
-const foundNumbers = [];
-const toRemember = document.getElementById("to-remember");
-const secondDelay = 30000;
 
-// Il computer genera 5 numeri random e li inserisce in un array
-while(cpuNumbers.length < 5){
-    let generator = getRndInteger(1, 100);
-    if(!cpuNumbers.includes(generator)){
-        cpuNumbers.push(generator);
-    }
-}
-console.log(`Stringa da ricordare: ${cpuNumbers}`);
-toRemember.innerHTML = `Stringa da ricordare: ${cpuNumbers}`;
+const playGame = document.getElementById("play-game");
 
-setTimeout(function(){
-    toRemember.classList.add("d-none");
-}, secondDelay)
+playGame.addEventListener("click", function(){
+    const userNumbers = [];
+    const foundNumbers = [];
+    const toRemember = document.getElementById("to-remember");
+    const secondDelay = 4000;
 
-// Questa funzione comincia con un delay di 30 secondi
-setTimeout(function(){
-    // L'user deve inserire i 5 numeri che ricorda
-    for (let i = 1; i <= 5; i++) {
-        let userInput = Number(prompt("Inserisci un numero"));
-        userNumbers.push(userInput);
-    }
-    console.log(`Numeri inseriti: ${userNumbers}`);
-    document.getElementById("user-input").innerHTML = `Numeri inseriti: ${userNumbers}`;
+    document.getElementById("user-input").innerHTML = ``;
+    document.getElementById("found-numbers").innerHTML = ``;
 
-    // Qua faccio il check per vedere se i numeri che ho inserito sono contenuti nell'array della cpu
-    for (let i = 0; i < 5; i++) {
-        if(cpuNumbers.includes(userNumbers[i])){
-            foundNumbers.push(userNumbers[i]);
+    // Il computer genera 5 numeri random e li inserisce in un array
+    const cpuNumbers = numberGenerator(5);
+    console.log(`Stringa da ricordare: ${cpuNumbers}`);
+    toRemember.innerHTML = `Stringa da ricordare: ${cpuNumbers}`;
+
+    setTimeout(function(){
+        toRemember.classList.add("d-none");
+    }, secondDelay)
+
+    // Questa funzione comincia con un delay di 30 secondi
+    setTimeout(function(){
+        // L'user deve inserire i 5 numeri che ricorda
+        for (let i = 1; i <= 5; i++) {
+            let userInput = Number(prompt("Inserisci un numero"));
+            userNumbers.push(userInput);
         }
-    }
+        console.log(`Numeri inseriti: ${userNumbers}`);
+        document.getElementById("user-input").innerHTML = `Numeri inseriti: ${userNumbers}`;
 
-    // Controllo di aver indovinato almeno un numero, altrimenti stampo nessun numero
-    if(foundNumbers.length === 0){
-        console.log("Nessun numero indovinato, ritenta!")
-        document.getElementById("found-numbers").innerHTML = `Nessun numero indovinato, ritenta!`;
+        // Qua faccio il check per vedere se i numeri che ho inserito sono contenuti nell'array della cpu
+        for (let i = 0; i < 5; i++) {
+            if(cpuNumbers.includes(userNumbers[i])){
+                foundNumbers.push(userNumbers[i]);
+            }
+        }
 
-    } else {
-        console.log(`Numeri indovinati: ${foundNumbers}`);
-        document.getElementById("found-numbers").innerHTML = `Numeri indovinati: ${foundNumbers}`;
-    }
+        // Controllo di aver indovinato almeno un numero, altrimenti stampo nessun numero
+        if(foundNumbers.length === 0){
+            console.log("Nessun numero indovinato, ritenta!")
+            document.getElementById("found-numbers").innerHTML = `Nessun numero indovinato, ritenta!`;
 
-    toRemember.classList.remove("d-none");
-}, secondDelay + 100);
+        } else {
+            console.log(`Numeri indovinati: ${foundNumbers}`);
+            document.getElementById("found-numbers").innerHTML = `Numeri indovinati: ${foundNumbers}`;
+        }
+
+        toRemember.classList.remove("d-none");
+    }, secondDelay + 100);
+});
+
+
 
 
 
