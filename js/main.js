9a05b64773e9594ee83056eb985316df1d9e34f9
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-04 17:34:48 +0100

    Create le setimeout Function: - Adesso la stringa da ricordare rimane impressa per 30 secondi - Passati i secondi la stringa scompare e il browser ci da la possibilità di inserire i 5 numeri

--- js/main.js
+++ js/main.js
@@ -23,7 +23,10 @@ const cpuNumbers = [];
 const container = document.getElementById("container");
 const userNumbers = [];
 const foundNumbers = [];
+const toRemember = document.getElementById("to-remember");
+const secondDelay = 30000;
 
+// Il computer genera 5 numeri random e li inserisce in un array
 while(cpuNumbers.length < 5){
     let generator = getRndInteger(1, 100);
     if(!cpuNumbers.includes(generator)){
@@ -31,27 +34,41 @@ while(cpuNumbers.length < 5){
     }
 }
 console.log(`Stringa da ricordare: ${cpuNumbers}`);
-document.getElementById("to-remember").innerHTML = `Stringa da ricordare: ${cpuNumbers}`
+toRemember.innerHTML = `Stringa da ricordare: ${cpuNumbers}`;
 
-for (let i = 1; i <= 5; i++) {
-    let userInput = Number(prompt("Inserisci un numero"));
-    userNumbers.push(userInput);
-}
-console.log(`Numeri inseriti: ${userNumbers}`);
-document.getElementById("user-input").innerHTML = `Numeri inseriti: ${userNumbers}`
+setTimeout(function(){
+    toRemember.classList.add("d-none");
+}, secondDelay)
 
-for (let i = 0; i < 5; i++) {
-    if(cpuNumbers.includes(userNumbers[i])){
-        foundNumbers.push(userNumbers[i]);
+// Questa funzione comincia con un delay di 30 secondi
+setTimeout(function(){
+    // L'user deve inserire i 5 numeri che ricorda
+    for (let i = 1; i <= 5; i++) {
+        let userInput = Number(prompt("Inserisci un numero"));
+        userNumbers.push(userInput);
     }
-}
+    console.log(`Numeri inseriti: ${userNumbers}`);
+    document.getElementById("user-input").innerHTML = `Numeri inseriti: ${userNumbers}`;
 
-if(foundNumbers.length === 0){
-    console.log("Nessun numero indovinato, ritenta!")
-    document.getElementById("found-numbers").innerHTML = `Nessun numero indovinato, ritenta!`;
+    // Qua faccio il check per vedere se i numeri che ho inserito sono contenuti nell'array della cpu
+    for (let i = 0; i < 5; i++) {
+        if(cpuNumbers.includes(userNumbers[i])){
+            foundNumbers.push(userNumbers[i]);
+        }
+    }
 
-} else {
-    console.log(`Numeri indovinati: ${foundNumbers}`);
-    document.getElementById("found-numbers").innerHTML = `Numeri indovinati: ${foundNumbers}`;
-}
+    // Controllo di aver indovinato almeno un numero, altrimenti stampo nessun numero
+    if(foundNumbers.length === 0){
+        console.log("Nessun numero indovinato, ritenta!")
+        document.getElementById("found-numbers").innerHTML = `Nessun numero indovinato, ritenta!`;
+
+    } else {
+        console.log(`Numeri indovinati: ${foundNumbers}`);
+        document.getElementById("found-numbers").innerHTML = `Numeri indovinati: ${foundNumbers}`;
+    }
+
+    toRemember.classList.remove("d-none");
+}, secondDelay + 100);
+
+
 
